025d9a18e9f091fb8a8789d21cf2a0f09b266e2c
Angelo Ocana <angeloocana@gmail.com>
2017-03-29 17:25:41 +0000

    remove old typescript conf files

--- src/typings/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare module ptzUserRepository {
-    export function UserRepository(db):IUserRepository;
-}
-
-declare module "ptz-user-repository"
-{
-    export = ptzUserRepository;
-}
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,5 +1,3 @@
-/// <reference path="../node_modules/ptz-user-domain/src/typings/Index.d.ts" />
-/// <reference path="../node_modules/ptz-assert/src/typings/index.d.ts" />
 /// <reference path="globals/mocha/index.d.ts" />
 /// <reference path="modules/bcryptjs/index.d.ts" />
 /// <reference path="modules/mongodb/index.d.ts" />
