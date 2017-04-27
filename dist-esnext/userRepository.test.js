f3063cd6a2c0424bc004d084f31055b6ce2207fa
Angelo Ocana <angeloocana@gmail.com>
2017-04-26 22:12:34 -0300

    Add tests

--- dist-esnext/userRepository.test.js
+++ dist-esnext/userRepository.test.js
@@ -1,12 +1,12 @@
 import { MongoClient } from 'mongodb';
-import { equal, ok } from 'ptz-assert';
+import { emptyArray, equal, notEqual, notOk, ok } from 'ptz-assert';
 import { User } from 'ptz-user-domain';
-import UserRepository from './userRepository';
+import { UserRepository } from './index';
 const MONGO_URL = 'mongodb://localhost:27017/test';
 var db, userRepository;
 describe('UserRepository', () => {
     beforeEach(async () => {
-        db = await MongoClient.connect(MONGO_URL);
+        db = await MongoClient.connect(MONGO_URL + Math.random().toString().replace('.', ''));
         userRepository = new UserRepository(db);
     });
     describe('save', () => {
@@ -26,19 +26,82 @@ describe('UserRepository', () => {
         });
         it('update');
     });
-    describe('find', () => {
-        it('limit by 3');
-        it('limit by 5');
-    });
     describe('getOtherUsersWithSameUserNameOrEmail', () => {
-        it('find by email');
-        it('find by userName');
-        it('not found');
+        it('find by email', async () => {
+            const user = new User({
+                displayName: 'Angelo',
+                email: 'angeloocana@gmail.com',
+                userName: 'angeloocana'
+            });
+            const user2 = new User({
+                displayName: 'Angelo',
+                email: 'angeloocana@gmail.com',
+                userName: 'gsgdsgsd'
+            });
+            await userRepository.save(user);
+            const userDb = await userRepository.getOtherUsersWithSameUserNameOrEmail(user2);
+            equal(userDb[0].email, user2.email);
+            notEqual(userDb[0].id, user2.id);
+        });
+        it('find by userName', async () => {
+            const user = new User({
+                displayName: 'Angelo',
+                email: 'angeloocana@gmail.com',
+                userName: 'angeloocana'
+            });
+            const user2 = new User({
+                displayName: 'Angelo',
+                email: 'advadvdavad@gmail.com',
+                userName: 'angeloocana'
+            });
+            await userRepository.save(user);
+            const userDb = await userRepository.getOtherUsersWithSameUserNameOrEmail(user2);
+            equal(userDb[0].userName, user2.userName);
+            notEqual(userDb[0].id, user2.id);
+        });
+        it('not found', async () => {
+            const user = new User({
+                displayName: 'Angelo',
+                email: 'dgh3t4hd@gmail.com',
+                userName: 'dgh3t4hd'
+            });
+            const userDb = await userRepository.getOtherUsersWithSameUserNameOrEmail(user);
+            emptyArray(userDb);
+        });
     });
     describe('getByUserNameOrEmail', () => {
-        it('find by email');
-        it('find by userName');
-        it('not found');
+        it('find by email', async () => {
+            const user = new User({
+                displayName: 'Angelo',
+                email: 'angeloocana@gmail.com',
+                userName: 'angeloocana'
+            });
+            await userRepository.save(user);
+            const userDb = await userRepository.getByUserNameOrEmail('angeloocana@gmail.com');
+            ok(userDb);
+            equal(userDb.id, user.id);
+            equal(userDb.displayName, user.displayName);
+            equal(userDb.email, user.email);
+            equal(userDb.userName, user.userName);
+        });
+        it('find by userName', async () => {
+            const user = new User({
+                displayName: 'Angelo',
+                email: 'angeloocana@gmail.com',
+                userName: 'angeloocana'
+            });
+            await userRepository.save(user);
+            const userDb = await userRepository.getByUserNameOrEmail('angeloocana');
+            ok(userDb);
+            equal(userDb.id, user.id);
+            equal(userDb.displayName, user.displayName);
+            equal(userDb.email, user.email);
+            equal(userDb.userName, user.userName);
+        });
+        it('not found', async () => {
+            const userDb = await userRepository.getByUserNameOrEmail('dgdsfsfbsf');
+            notOk(userDb);
+        });
     });
 });
 //# sourceMappingURL=userRepository.test.js.map
--- dist/userRepository.test.js
+++ dist/userRepository.test.js
@@ -6,11 +6,7 @@ var _ptzAssert = require('ptz-assert');
 
 var _ptzUserDomain = require('ptz-user-domain');
 
-var _userRepository = require('./userRepository');
-
-var _userRepository2 = _interopRequireDefault(_userRepository);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+var _index = require('./index');
 
 function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
 
@@ -23,12 +19,12 @@ describe('UserRepository', function () {
                 switch (_context.prev = _context.next) {
                     case 0:
                         _context.next = 2;
-                        return _mongodb.MongoClient.connect(MONGO_URL);
+                        return _mongodb.MongoClient.connect(MONGO_URL + Math.random().toString().replace('.', ''));
 
                     case 2:
                         db = _context.sent;
 
-                        userRepository = new _userRepository2.default(db);
+                        userRepository = new _index.UserRepository(db);
 
                     case 4:
                     case 'end':
@@ -74,19 +70,196 @@ describe('UserRepository', function () {
         })));
         it('update');
     });
-    describe('find', function () {
-        it('limit by 3');
-        it('limit by 5');
-    });
     describe('getOtherUsersWithSameUserNameOrEmail', function () {
-        it('find by email');
-        it('find by userName');
-        it('not found');
+        it('find by email', _asyncToGenerator(regeneratorRuntime.mark(function _callee3() {
+            var user, user2, userDb;
+            return regeneratorRuntime.wrap(function _callee3$(_context3) {
+                while (1) {
+                    switch (_context3.prev = _context3.next) {
+                        case 0:
+                            user = new _ptzUserDomain.User({
+                                displayName: 'Angelo',
+                                email: 'angeloocana@gmail.com',
+                                userName: 'angeloocana'
+                            });
+                            user2 = new _ptzUserDomain.User({
+                                displayName: 'Angelo',
+                                email: 'angeloocana@gmail.com',
+                                userName: 'gsgdsgsd'
+                            });
+                            _context3.next = 4;
+                            return userRepository.save(user);
+
+                        case 4:
+                            _context3.next = 6;
+                            return userRepository.getOtherUsersWithSameUserNameOrEmail(user2);
+
+                        case 6:
+                            userDb = _context3.sent;
+
+                            (0, _ptzAssert.equal)(userDb[0].email, user2.email);
+                            (0, _ptzAssert.notEqual)(userDb[0].id, user2.id);
+
+                        case 9:
+                        case 'end':
+                            return _context3.stop();
+                    }
+                }
+            }, _callee3, undefined);
+        })));
+        it('find by userName', _asyncToGenerator(regeneratorRuntime.mark(function _callee4() {
+            var user, user2, userDb;
+            return regeneratorRuntime.wrap(function _callee4$(_context4) {
+                while (1) {
+                    switch (_context4.prev = _context4.next) {
+                        case 0:
+                            user = new _ptzUserDomain.User({
+                                displayName: 'Angelo',
+                                email: 'angeloocana@gmail.com',
+                                userName: 'angeloocana'
+                            });
+                            user2 = new _ptzUserDomain.User({
+                                displayName: 'Angelo',
+                                email: 'advadvdavad@gmail.com',
+                                userName: 'angeloocana'
+                            });
+                            _context4.next = 4;
+                            return userRepository.save(user);
+
+                        case 4:
+                            _context4.next = 6;
+                            return userRepository.getOtherUsersWithSameUserNameOrEmail(user2);
+
+                        case 6:
+                            userDb = _context4.sent;
+
+                            (0, _ptzAssert.equal)(userDb[0].userName, user2.userName);
+                            (0, _ptzAssert.notEqual)(userDb[0].id, user2.id);
+
+                        case 9:
+                        case 'end':
+                            return _context4.stop();
+                    }
+                }
+            }, _callee4, undefined);
+        })));
+        it('not found', _asyncToGenerator(regeneratorRuntime.mark(function _callee5() {
+            var user, userDb;
+            return regeneratorRuntime.wrap(function _callee5$(_context5) {
+                while (1) {
+                    switch (_context5.prev = _context5.next) {
+                        case 0:
+                            user = new _ptzUserDomain.User({
+                                displayName: 'Angelo',
+                                email: 'dgh3t4hd@gmail.com',
+                                userName: 'dgh3t4hd'
+                            });
+                            _context5.next = 3;
+                            return userRepository.getOtherUsersWithSameUserNameOrEmail(user);
+
+                        case 3:
+                            userDb = _context5.sent;
+
+                            (0, _ptzAssert.emptyArray)(userDb);
+
+                        case 5:
+                        case 'end':
+                            return _context5.stop();
+                    }
+                }
+            }, _callee5, undefined);
+        })));
     });
     describe('getByUserNameOrEmail', function () {
-        it('find by email');
-        it('find by userName');
-        it('not found');
+        it('find by email', _asyncToGenerator(regeneratorRuntime.mark(function _callee6() {
+            var user, userDb;
+            return regeneratorRuntime.wrap(function _callee6$(_context6) {
+                while (1) {
+                    switch (_context6.prev = _context6.next) {
+                        case 0:
+                            user = new _ptzUserDomain.User({
+                                displayName: 'Angelo',
+                                email: 'angeloocana@gmail.com',
+                                userName: 'angeloocana'
+                            });
+                            _context6.next = 3;
+                            return userRepository.save(user);
+
+                        case 3:
+                            _context6.next = 5;
+                            return userRepository.getByUserNameOrEmail('angeloocana@gmail.com');
+
+                        case 5:
+                            userDb = _context6.sent;
+
+                            (0, _ptzAssert.ok)(userDb);
+                            (0, _ptzAssert.equal)(userDb.id, user.id);
+                            (0, _ptzAssert.equal)(userDb.displayName, user.displayName);
+                            (0, _ptzAssert.equal)(userDb.email, user.email);
+                            (0, _ptzAssert.equal)(userDb.userName, user.userName);
+
+                        case 11:
+                        case 'end':
+                            return _context6.stop();
+                    }
+                }
+            }, _callee6, undefined);
+        })));
+        it('find by userName', _asyncToGenerator(regeneratorRuntime.mark(function _callee7() {
+            var user, userDb;
+            return regeneratorRuntime.wrap(function _callee7$(_context7) {
+                while (1) {
+                    switch (_context7.prev = _context7.next) {
+                        case 0:
+                            user = new _ptzUserDomain.User({
+                                displayName: 'Angelo',
+                                email: 'angeloocana@gmail.com',
+                                userName: 'angeloocana'
+                            });
+                            _context7.next = 3;
+                            return userRepository.save(user);
+
+                        case 3:
+                            _context7.next = 5;
+                            return userRepository.getByUserNameOrEmail('angeloocana');
+
+                        case 5:
+                            userDb = _context7.sent;
+
+                            (0, _ptzAssert.ok)(userDb);
+                            (0, _ptzAssert.equal)(userDb.id, user.id);
+                            (0, _ptzAssert.equal)(userDb.displayName, user.displayName);
+                            (0, _ptzAssert.equal)(userDb.email, user.email);
+                            (0, _ptzAssert.equal)(userDb.userName, user.userName);
+
+                        case 11:
+                        case 'end':
+                            return _context7.stop();
+                    }
+                }
+            }, _callee7, undefined);
+        })));
+        it('not found', _asyncToGenerator(regeneratorRuntime.mark(function _callee8() {
+            var userDb;
+            return regeneratorRuntime.wrap(function _callee8$(_context8) {
+                while (1) {
+                    switch (_context8.prev = _context8.next) {
+                        case 0:
+                            _context8.next = 2;
+                            return userRepository.getByUserNameOrEmail('dgdsfsfbsf');
+
+                        case 2:
+                            userDb = _context8.sent;
+
+                            (0, _ptzAssert.notOk)(userDb);
+
+                        case 4:
+                        case 'end':
+                            return _context8.stop();
+                    }
+                }
+            }, _callee8, undefined);
+        })));
     });
 });
 //# sourceMappingURL=userRepository.test.js.map
