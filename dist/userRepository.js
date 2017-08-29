fb937627761b4eaafd6afebadd2c54bcefb06f5a
Alan Marcell <alan.marcell@accenture.com>
2017-08-28 23:35:01 -0300

    Use node 6 on travis

--- dist/userRepository.js
+++ dist/userRepository.js
@@ -13,21 +13,29 @@ var _ramda2 = _interopRequireDefault(_ramda);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-const createUserRepository = exports.createUserRepository = _ramda2.default.curry(async (collectionName, url) => {
-    const db = await (0, _ptzCoreRepository.getDb)(url);
-    const collection = (0, _ptzCoreRepository.getDbCollection)(db, collectionName);
-    return {
-        db,
-        collectionName,
-        save: (0, _ptzCoreRepository.save)(collection),
-        getByIds: (0, _ptzCoreRepository.getByIds)(collection),
-        find: (0, _ptzCoreRepository.find)(collection),
-        getById: (0, _ptzCoreRepository.getById)(collection),
-        getDbCollection: _ptzCoreRepository.getDbCollection,
-        getOtherUsersWithSameUserNameOrEmail: getOtherUsersWithSameUserNameOrEmail(collection),
-        getByUserNameOrEmail: getByUserNameOrEmail(collection)
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+
+const createUserRepository = exports.createUserRepository = _ramda2.default.curry((() => {
+    var _ref = _asyncToGenerator(function* (collectionName, url) {
+        const db = yield (0, _ptzCoreRepository.getDb)(url);
+        const collection = (0, _ptzCoreRepository.getDbCollection)(db, collectionName);
+        return {
+            db,
+            collectionName,
+            save: (0, _ptzCoreRepository.save)(collection),
+            getByIds: (0, _ptzCoreRepository.getByIds)(collection),
+            find: (0, _ptzCoreRepository.find)(collection),
+            getById: (0, _ptzCoreRepository.getById)(collection),
+            getDbCollection: _ptzCoreRepository.getDbCollection,
+            getOtherUsersWithSameUserNameOrEmail: getOtherUsersWithSameUserNameOrEmail(collection),
+            getByUserNameOrEmail: getByUserNameOrEmail(collection)
+        };
+    });
+
+    return function (_x, _x2) {
+        return _ref.apply(this, arguments);
     };
-});
+})());
 const getOtherUsersWithSameUserNameOrEmail = _ramda2.default.curry((collection, user) => {
     const query = {
         _id: { $ne: user.id },
--- dist/userRepository.test.js
+++ dist/userRepository.test.js
@@ -8,14 +8,16 @@ var Core = _interopRequireWildcard(_index);
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+
 const MONGO_URL = 'mongodb://localhost:27017/ptz-core-repo';
 var userRepository;
 describe('UserRepository', () => {
-    beforeEach(async () => {
-        userRepository = await Core.createUserRepository('test-collection', MONGO_URL);
-    });
+    beforeEach(_asyncToGenerator(function* () {
+        userRepository = yield Core.createUserRepository('test-collection', MONGO_URL);
+    }));
     describe('getOtherUsersWithSameUserNameOrEmail', () => {
-        it('find by email', async () => {
+        it('find by email', _asyncToGenerator(function* () {
             const user = {
                 id: 'testid',
                 displayName: 'Angelo',
@@ -28,12 +30,12 @@ describe('UserRepository', () => {
                 email: 'angeloocana@gmail.com',
                 userName: 'gsgdsgsd'
             };
-            await userRepository.save(user);
-            const userDb = await userRepository.getOtherUsersWithSameUserNameOrEmail(user2);
+            yield userRepository.save(user);
+            const userDb = yield userRepository.getOtherUsersWithSameUserNameOrEmail(user2);
             (0, _ptzAssert.equal)(userDb[0].email, user2.email);
             (0, _ptzAssert.notEqual)(userDb[0].id, user2.id);
-        });
-        it('find by userName', async () => {
+        }));
+        it('find by userName', _asyncToGenerator(function* () {
             const user = {
                 id: 'testid',
                 displayName: 'Angelo',
@@ -46,57 +48,57 @@ describe('UserRepository', () => {
                 email: 'advadvdavad@gmail.com',
                 userName: 'angeloocana'
             };
-            await userRepository.save(user);
-            const userDb = await userRepository.getOtherUsersWithSameUserNameOrEmail(user2);
+            yield userRepository.save(user);
+            const userDb = yield userRepository.getOtherUsersWithSameUserNameOrEmail(user2);
             (0, _ptzAssert.equal)(userDb[0].userName, user2.userName);
             (0, _ptzAssert.notEqual)(userDb[0].id, user2.id);
-        });
-        it('not found', async () => {
+        }));
+        it('not found', _asyncToGenerator(function* () {
             const user = {
                 id: 'testid',
                 displayName: 'Angelo',
                 email: 'dgh3t4hd@gmail.com',
                 userName: 'dgh3t4hd'
             };
-            const userDb = await userRepository.getOtherUsersWithSameUserNameOrEmail(user);
+            const userDb = yield userRepository.getOtherUsersWithSameUserNameOrEmail(user);
             (0, _ptzAssert.emptyArray)(userDb);
-        });
+        }));
     });
     describe('getByUserNameOrEmail', () => {
-        it('find by email', async () => {
+        it('find by email', _asyncToGenerator(function* () {
             const user = {
                 id: 'testid',
                 displayName: 'Angelo',
                 email: 'angeloocana@gmail.com',
                 userName: 'angeloocana'
             };
-            await userRepository.save(user);
-            const userDb = await userRepository.getByUserNameOrEmail('angeloocana@gmail.com');
+            yield userRepository.save(user);
+            const userDb = yield userRepository.getByUserNameOrEmail('angeloocana@gmail.com');
             (0, _ptzAssert.ok)(userDb);
             (0, _ptzAssert.equal)(userDb.id, user.id);
             (0, _ptzAssert.equal)(userDb.displayName, user.displayName);
             (0, _ptzAssert.equal)(userDb.email, user.email);
             (0, _ptzAssert.equal)(userDb.userName, user.userName);
-        });
-        it('find by userName', async () => {
+        }));
+        it('find by userName', _asyncToGenerator(function* () {
             const user = {
                 id: 'testid',
                 displayName: 'Angelo',
                 email: 'angeloocana@gmail.com',
                 userName: 'angeloocana'
             };
-            await userRepository.save(user);
-            const userDb = await userRepository.getByUserNameOrEmail('angeloocana');
+            yield userRepository.save(user);
+            const userDb = yield userRepository.getByUserNameOrEmail('angeloocana');
             (0, _ptzAssert.ok)(userDb);
             (0, _ptzAssert.equal)(userDb.id, user.id);
             (0, _ptzAssert.equal)(userDb.displayName, user.displayName);
             (0, _ptzAssert.equal)(userDb.email, user.email);
             (0, _ptzAssert.equal)(userDb.userName, user.userName);
-        });
-        it('not found', async () => {
-            const userDb = await userRepository.getByUserNameOrEmail('dgdsfsfbsf');
+        }));
+        it('not found', _asyncToGenerator(function* () {
+            const userDb = yield userRepository.getByUserNameOrEmail('dgdsfsfbsf');
             (0, _ptzAssert.notOk)(userDb);
-        });
+        }));
     });
 });
 //# sourceMappingURL=userRepository.test.js.map
